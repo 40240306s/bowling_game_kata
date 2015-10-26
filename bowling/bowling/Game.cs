d271955cb3d4e1e75c0ce317c68f8cedf0897268
40240306s <40240306s@ntnu.edu.tw>
2015-10-26 11:12:09 +0800

    OK

--- bowling/bowling/Game.cs
+++ bowling/bowling/Game.cs
@@ -7,32 +7,37 @@ namespace bowling
 {
     class Game
     {
-    
+
         int Now = 0;
-        int[] B = new int[21];
+        int[] B = new int[30];
         internal void SetPins(int p)
         {
             B[Now] = p;
-            ++Now;
-            
-           
+            if (Now < 18&&p==10)
+                Now += 2;
+            else Now += 1;
+
+
         }
 
         internal int GetFinalScore()
         {
-            int score = 0,A=0;
-            for (int i=0;i<21;++i)
+            int score = 0;
+            for (int i = 0; i < 18; ++i)
             {
 
                 score += B[i];
-              /*  if (i %2==1)
-                { 
-                if()
-                
-                }*/
+                if (i % 2 == 1&&B[i] + B[i - 1] == 10)
+                {
+                        score += B[i + 1];
+                }
+                if(B[i]==10&&i%2==0)
+                {
+                        score += B[i + 3];
+                }
             }
+            score += B[18] + B[19] + B[20];
 
-         
             return score;
         }
     }
--- bowling/bowling/TestBowling.cs
+++ bowling/bowling/TestBowling.cs
@@ -23,7 +23,7 @@ namespace bowling
         [Test]
         public void ASpair()
         {
-          /*  Game g = new Game();
+           Game g = new Game();
             g.SetPins(5);
             g.SetPins(5);
             g.SetPins(5);
@@ -31,9 +31,77 @@ namespace bowling
             {
                 g.SetPins(0);
             }
-            Assert.That(g.GetFinalScore(), Is.EqualTo(20));*/
+            Assert.That(g.GetFinalScore(), Is.EqualTo(20));
         }
+        [Test]
+        public void AStrike()
+        {
+            Game g = new Game();
+            g.SetPins(10);
+     
+            g.SetPins(3);
+            g.SetPins(4);
+            for (int i = 0; i < 16; i++)
+            {
+                g.SetPins(0);
+            }
+            Assert.That(g.GetFinalScore(), Is.EqualTo(24));
+        }
+        [Test]
+        public void StrikeSpair()
+        {
+            Game g = new Game();
+            g.SetPins(10);
 
+            g.SetPins(3);
+            g.SetPins(7);
+            g.SetPins(5);
+            g.SetPins(0);
+            for (int i = 0; i < 14; i++)
+            {
+                g.SetPins(0);
+            }
+            Assert.That(g.GetFinalScore(), Is.EqualTo(40));
+        }
+        [Test]
+        public void FinalSpairCase()
+        {
+            Game g = new Game();
+            for(int i=0;i<18;i+=1)
+            {
+                g.SetPins(0);
+            }
+            g.SetPins(0);
+            g.SetPins(10);
+            g.SetPins(5);
+            Assert.That(g.GetFinalScore(), Is.EqualTo(15));
+        }
+        [Test]
+        public void Ptt()
+        {
+            Game g = new Game();
+            g.SetPins(1);
+            g.SetPins(4);
+            g.SetPins(4);
+            g.SetPins(5);
+            g.SetPins(6);
+            g.SetPins(4);
+            g.SetPins(5);
+            g.SetPins(5);
+            g.SetPins(10);
+            g.SetPins(0);
+            g.SetPins(1);
+            g.SetPins(7);
+            g.SetPins(3); 
+            g.SetPins(6);
+            g.SetPins(4);
+            g.SetPins(10);
+            g.SetPins(2);
+            g.SetPins(8);
+            g.SetPins(6);
+            Assert.That(g.GetFinalScore(), Is.EqualTo(133));
+
+        }
 
 
 
